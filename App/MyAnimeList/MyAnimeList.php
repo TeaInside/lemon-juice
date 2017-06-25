8f8838a84289504918f28e1457ab5621acb1244a
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-25 13:33:28 +0700

    Update MyAnimeList class

--- App/MyAnimeList/MyAnimeList.php
+++ App/MyAnimeList/MyAnimeList.php
@@ -10,10 +10,35 @@ namespace App\MyAnimeList;
 
 use Lemon\Curl;
 use App\MyAnimeList\MyAnimeListContract;
+use App\MyAnimeList\MyAnimeListException;
 
 defined("data") or die("Data not defined !");
 
 class MyAnimeList implements MyAnimeListContract
 {
+	/**
+	 * @var array
+	 */
+	private $hash_table = array();
 
+	/**
+	 * Constructor.
+	 */
+	public function __construct()
+	{
+		is_dir(data) or mkdir(data);
+		is_dir(data."/MyAnimeList") or mkdir(data."/MyAnimeList");
+		is_dir(data."/MyAnimeList/history") or mkdir(data."/MyAnimeList/history");
+		is_dir(data."/MyAnimeList/data") or mkdir(data."/MyAnimeList/data");
+		if (!(is_dir(data."/MyAnimeList") && is_dir(data."/MyAnimeList/history") && is_dir(data."/MyAnimeList/data"))) {
+			throw new MyAnimeListException("Cannot create directory.", 1);
+		}
+		if (file_exists(data."/MyAnimeList/hash_table.json")) {
+			$this->hash_table = json_decode(file_get_contents(data."/MyAnimeList/hash_table.json"), true);
+			$this->hash_table = is_array($this->hash_table) ? $this->hash_table : array();
+		} else {
+			file_put_contents(data."/MyAnimeList/hash_table.json", json_encode(array()));
+			$this->hash_table = array();
+		}
+	}
 }
--- App/MyAnimeList/MyAnimeListContract.php
+++ App/MyAnimeList/MyAnimeListContract.php
@@ -4,5 +4,13 @@ namespace App\MyAnimeList;
 
 interface MyAnimeListContract
 {
+	/**
+	 * Constructor.
+	 */
+	public function __construct();
+
+	/**
+	 * Execute anime search.
+	 */
 	public function exec();
 }
