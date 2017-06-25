52099af26d5efeffdfaf36b552ea81f80a5dd35b
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-25 15:04:02 +0700

    Add test.php

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+require __DIR__."/vendor/autoload.php";
+
+use App\MyAnimeList\MyAnimeList;
+
+define("data", __DIR__."/data");
+
+$a = new MyAnimeList("ammarfaizi2", "triosemut123");
+#$a->search("naruto");
+#$a->exec();
+#$aa = $a->get_info(20);
+$aa = $a->simple_search("naruto ");
+
+var_dump($aa);
