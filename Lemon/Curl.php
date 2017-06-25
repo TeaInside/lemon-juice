8c91009ce3f34c2b353747f83ee318ecc5cadbcb
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-25 12:57:28 +0700

    Add some comments

--- Lemon/Curl.php
+++ Lemon/Curl.php
@@ -14,27 +14,39 @@ class Curl
 	const USERAGENT = "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:46.0) Gecko/20100101 Firefox/46.0";
 
 	/**
+	 * Curl resource.
+	 *
+	 * @var curl
+	 */
+	private $ch;
+
+	/**
+	 * URL.
+	 *
 	 * @var string
 	 */
 	private $url;
 
 	/**
+	 * Curl option.
+	 *
 	 * @var array
 	 */
 	private $opt = array();
+
 
 	/**
 	 * Constructor.
 	 */
 	public function __construct($url)
 	{
+		$this->ch  = curl_init();
 		$this->url = $url;
 		$this->opt = array(
 				CURLOPT_RETURNTRANSFER => true,
 				CURLOPT_SSL_VERIFYHOST => false,
 				CURLOPT_SSL_VERIFYPEER => false,
 				CURLOPT_USERAGENT	   => self::USERAGENT,
-
 			);
 	}
 
