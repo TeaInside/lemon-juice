0510d93ee5700a371c0aa866ae78bba485876cb5
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-28 00:34:46 +0700

    PHP CS Fixer

--- Bot/BotContracts/TelegramContract.php
+++ Bot/BotContracts/TelegramContract.php
@@ -6,14 +6,14 @@ interface TelegramContract
 {
     /**
      * Constructor.
-     * 
+     *
      * @param string $token
      */
     public function __construct($token);
 
     /**
      * Run.
-     * 
+     *
      * @param string $token
      */
     public static function run($token);
