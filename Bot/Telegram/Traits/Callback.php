3a1147ad3b593fb5edc5b960c9d0fed21737f72a
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-02 16:30:41 +0700

    Test cancel_user_warning

--- Bot/Telegram/Traits/Callback.php
+++ Bot/Telegram/Traits/Callback.php
@@ -36,6 +36,35 @@ trait Callback
      */
     private function parseCallback()
     {
-        
+        $a = json_decode($this->callback_data, true);
+        #var_dump($a);
+        $callback_cmd = array(
+                "rw",
+                "cw"
+            );
+        switch ($a['cmd']) {
+            case 'rw':
+                    $this->cancel_warning($a['c']);
+                break;
+            case 'cw':
+
+                break;
+            default:
+                # code...
+                break;
+        }
+    }
+
+    private function cancel_warning($uifo)
+    {
+        $a = json_decode(file_get_contents(storage."/telegram/user_warning_data.txt"), true);
+        if (isset($a[$uifo])) {
+            $a[$uifo] = $a[$uifo]-1;
+            file_put_contents(storage."/telegram/user_warning_data.txt", json_encode($a, 128));
+            $msg = "Berhasil membatalkan peringatan.\n\nJumlah peringatan {user} sekarang <b>".($a[$uifo])."</b>";
+        } else {
+            $msg = "Action cancel_warning failed !";
+        }
+        $this->textReply($msg, null, null, array("parse_mode"=>"HTML"));
     }
 }
