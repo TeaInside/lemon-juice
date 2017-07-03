1db6ec07f1a57b7af6d0c157187c9ca8cd4ce025
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-03 16:24:57 +0700

    Update JAVAVIRTUAL_DIR constant

--- config.php
+++ config.php
@@ -8,8 +8,9 @@
 define("storage", __DIR__."/storage");
 define("data", __DIR__."/data");
 define("logs", __DIR__."/logs");
-define("PHPVIRTUAL_DIR", "/home/ammar/web/webhooks/public/php_virtual/");
-define("PHPVIRTUAL_URL", "https://webhooks.redangel.ga/php_virtual");
+define("PHPVIRTUAL_DIR", "/home/ammar/web/webhooks/public/virtual/php");
+define("PHPVIRTUAL_URL", "https://webhooks.redangel.ga/virtual/php");
+define("JAVAVIRTUAL_DIR", "/home/ammar/web/webhooks/public/virtual/java");
 
 is_dir(storage) or mkdir(storage);
 is_dir(data) or mkdir(data);
