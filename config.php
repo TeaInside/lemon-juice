7c3fb022559e77ed57835c83f757b79f6f4466c6
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-04 19:31:16 +0700

    Update RubyVirtual

--- config.php
+++ config.php
@@ -8,9 +8,10 @@
 define("storage", __DIR__."/storage");
 define("data", __DIR__."/data");
 define("logs", __DIR__."/logs");
-define("PHPVIRTUAL_DIR", "/home/ammar/web/webhooks/public/virtual/php");
 define("PHPVIRTUAL_URL", "https://webhooks.redangel.ga/virtual/php");
+define("PHPVIRTUAL_DIR", "/home/ammar/web/webhooks/public/virtual/php");
 define("JAVAVIRTUAL_DIR", "/home/ammar/web/webhooks/public/virtual/java");
+define("RUBYVIRTUAL_DIR", "/home/ammar/web/webhooks/public/virtual/ruby");
 
 is_dir(storage) or mkdir(storage);
 is_dir(data) or mkdir(data);
