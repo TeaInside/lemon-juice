8520e36888f3ae3da7075b30ea57aa867f4888a2
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-07 10:36:40 +0700

    Update

---
+++ Stack/LINE/LINE.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Stack\LINE;
+
+/**
+ * https://api.line.me/v2/bot/profile/
+ *
+ *
+ */
+
+
+
+class LINE
+{	
+	/**
+	 * @var string
+	 */
+	private $token;
+
+	/**
+	 * @var string
+	 */
+	private $channel_secret;
+
+	/**
+	 * @var array
+	 */
+	private $header;
+
+	/**
+	 * Constructor.
+	 * @param string $token
+	 * @param string $channel_secret
+	 */
+	public function __construct($token, $channel_secret)
+	{
+		$this->header = ["Authorization: Bearer $channelToken"];
+	}
+
+	/**
+	 * @param string $method
+	 * @param string $url
+	 * @param string $opt
+	 * @return string
+	 */
+	private function sendRequest($url, $method, $opt)
+	{
+		$st = new \Curl($url);
+		$st->set_opt([
+				CURLOPT_CUSTOMREQUEST => $method,
+				CURLOPT_BINARYTRANSFER => true,
+            	CURLOPT_HEADER => true,
+			]);
+	}
+}
