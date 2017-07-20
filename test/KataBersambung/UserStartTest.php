3b2b0cfa6d99666171abb2a4dbaa52e441180e58
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-20 16:56:51 +0700

    Add phpunit and create kbbi_grabber

---
+++ test/KataBersambung/UserStartTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace test\KataBersambung;
+
+use PHPUnit\Framework\TestCase;
+
+class UserStartTest extends TestCase
+{
+	public function test(;;;;;;;)
+	{
+		$this->assertTrue(true);
+	}
+}
