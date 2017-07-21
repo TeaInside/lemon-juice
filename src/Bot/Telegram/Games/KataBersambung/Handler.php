ebb8452c4f0e25690f47e27662dc84823f54c59b
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-21 13:30:15 +0700

    Update namespace

--- src/Bot/Telegram/Games/KataBersambung/Handler.php
+++ src/Bot/Telegram/Games/KataBersambung/Handler.php
@@ -7,8 +7,15 @@ namespace Bot\Telegram\Games\KataBersambung;
  * @package Bot\Telegram\Games\KataBersambung
  */
 
-use Bot\Telegram\Games\KataBersambung\HandlerContract;
+use Bot\Telegram\Games\KataBersambung\Session;
+use Bot\Telegram\Games\KataBersambung\Database;
+use Bot\Telegram\Games\KataBersambung\Contracts\HandlerContract;
 
 class Handler implements HandlerContract
 {
+	public function __construct()
+	{
+		$this->kdb 		= new Database();
+		$this->session	= new Session();
+	}
 }
