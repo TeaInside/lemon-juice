ec3006c8defa5651d2d511c38c8c73f20b5ce8ac
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-21 16:51:33 +0700

    Create '/party' command

--- src/Bot/Telegram/Traits/ContractParty.php
+++ src/Bot/Telegram/Traits/ContractParty.php
@@ -14,6 +14,6 @@ use Bot\Telegram\Games\KataBersambung\Handler;
  {
  	public function party()
  	{
- 		
+ 		$this->textReply("Sedang dalam perbaikan :3\n\nMohon dibantu https://github.com/ammarfaizi2/lemon-juice",null, $this->event['message']['message_id'], array("parse_mode"=>"HTML"));
  	}
  }
