0dfa317426681c5dc67d9f2a41d84f20950fa4d0
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-21 13:21:24 +0700

    Prepare new namespace

---
+++ src/Bot/Telegram/Games/KataBersambung/Session.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bot\Telegram\Games\KataBersambung;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com>
+ * @package Bot\Telegram\Games\KataBersambung
+ */
+
+use Bot\Telegram\Games\KataBersambung\Contracts\SessionContract;
+
+class Session implements SessionContract
+{
+}
