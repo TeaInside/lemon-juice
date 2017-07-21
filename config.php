29f2931a62ea471ed4547d4a777ec36fa3f12ea8
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-21 23:10:34 +0700

    Debug

--- config.php
+++ config.php
@@ -39,5 +39,7 @@ if (!defined("data")) {
      * PDO
      */
     define("PDO_CONNECT", "mysql:host=localhost;dbname=lemon_juice;port=3306");
-    define("PDO_USER", "icetea");
-    define("PDO_PASS", "triosemut123");
+    #define("PDO_USER", "icetea");
+    #define("PDO_PASS", "triosemut123");
+    define("PDO_USER", "debian-sys-maint");
+    define("PDO_PASS", "");
