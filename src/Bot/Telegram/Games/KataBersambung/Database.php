e30780e125b39dc1a5dc9a7ef344feeca905825c
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-21 13:34:42 +0700

    Create DB class and DB Contract

---
+++ src/Bot/Telegram/Games/KataBersambung/Database.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Bot\Telegram\Games\KataBersambung;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com>
+ * @package Bot\Telegram\Games\KataBersambung
+ */
+
+use PDO;
+use Bot\Telegram\Games\KataBersambung\Contracts\DatabaseContract;
+
+class Database implements DatabaseContract
+{	
+	/**
+	 * @var PDO
+	 */
+	private $pdo;
+
+	/** 
+	 * Constructor.
+	 */
+	public function __construct()
+	{
+		$this->pdo = new PDO(PDO_CONNECT);
+	}
+}
