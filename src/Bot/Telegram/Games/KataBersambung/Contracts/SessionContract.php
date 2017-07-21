3ac38f0177a61a25aa9b328da001a29f4fb20821
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-21 14:11:29 +0700

    Improve session class

--- src/Bot/Telegram/Games/KataBersambung/Contracts/SessionContract.php
+++ src/Bot/Telegram/Games/KataBersambung/Contracts/SessionContract.php
@@ -8,5 +8,5 @@ interface SessionContract
 	 * @param int 	 $room_id
 	 * @param string $type		(private,group)
 	 */
-	public function make_session($room_id, $type);
+	public function make_session($room_id, $type, $started_id, $room_name = null);
 }
