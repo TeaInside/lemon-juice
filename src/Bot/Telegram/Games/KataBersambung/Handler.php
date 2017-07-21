e2e566b5c8c9a76b52a40c87ab845d103c6c5012
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-21 13:24:29 +0700

    Make Handler class

---
+++ src/Bot/Telegram/Games/KataBersambung/Handler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bot\Telegram\Games\KataBersambung;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com>
+ * @package Bot\Telegram\Games\KataBersambung
+ */
+
+use Bot\Telegram\Games\KataBersambung\HandlerContract;
+
+class Handler implements HandlerContract
+{
+}
