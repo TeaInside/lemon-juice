d6a949ad9eab39d52ad0da2590e465b40ae32265
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-21 16:46:43 +0700

    Create ContractParty trait

---
+++ src/Bot/Telegram/Traits/ContractParty.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bot\Telegram\Traits;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com>
+ * @package Bot\Telegram\Traits
+ * @since 0.0.1
+ */
+
+ trait ContractParty
+ {
+ 	
+ }
