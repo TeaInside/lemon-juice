455a8d2edced9f3707db2aad412b9dff45148166
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-23 00:09:31 +0700

    Fix syntax error

--- src/App/VGMdb/VGMdb.php
+++ src/App/VGMdb/VGMdb.php
@@ -13,30 +13,29 @@ use App\VGMdb\VGMdbContract;
 
 class VGMdb implements VGMdbContract
 {
-	/**
-	 * API URL
-	 */
-	const API_URL = "https://vgmdb.info/";
+    /**
+     * API URL
+     */
+    const API_URL = "https://vgmdb.info/";
 
-	/**
-	 * Constructor.
-	 */
-	public function __construct()
-	{
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+    }
 
-	}
-
-	/**
-	 * @param string $q
-	 * @return string
-	 */
-	public function seacrh($q)
-	{
-		$ch = new Curl(self::API_URL."search/".urlencode($q));
-		$ch->set_opt(
-				CURLOPT_HTTPHEADER => array("Accept: application/json")
-			);
-		$out = $ch->exec();
-		return $out;
-	}
+    /**
+     * @param string $q
+     * @return string
+     */
+    public function seacrh($q)
+    {
+        $ch = new Curl(self::API_URL."search/".urlencode($q));
+        $ch->set_opt([
+                CURLOPT_HTTPHEADER => array("Accept: application/json")
+            ]);
+        $out = $ch->exec();
+        return $out;
+    }
 }
