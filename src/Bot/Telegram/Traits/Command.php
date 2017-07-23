aef79663f28762affa7a4ad377b23d0536070314
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-23 14:10:50 +0700

    Fix bug : Video long duration

--- src/Bot/Telegram/Traits/Command.php
+++ src/Bot/Telegram/Traits/Command.php
@@ -394,7 +394,7 @@ trait Command
                                 return (strlen($menit)==1 ? "0{$menit}" : "{$menit}").":".(strlen($detik)==1 ? "0{$detik}" : "{$detik}");
                             };
                             file_put_contents("debug_dur.txt", json_encode($dur));
-                            $x = $this->tel->sendVideo("https://webhooks.redangel.ga/.webhooks/telegram/video/".$hash_fn.".mp4", $this->room, "Berikut ini adalah cuplikan singkat dari anime yang mirip.\n\nDurasi : ".$fd($dur['start'])." - ".$fd($dur['end']), $this->event['message']['message_id']);
+                            $x = $this->tel->sendVideo("https://webhooks.redangel.ga/telegram/video/".$hash_fn.".mp4", $this->room, "Berikut ini adalah cuplikan singkat dari anime yang mirip.\n\nDurasi : ".$fd($dur['start'])." - ".$fd($dur['end']), $this->event['message']['message_id']);
                             file_put_contents("debug_video.txt", $x);
                         } else {
                             $this->textReply("Mohon maaf, pencarian tidak ditemukan !", null, $this->event['message']['message_id']);
