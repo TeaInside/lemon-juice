bdfc3ebaf72eb1edfed7708f0e97458e6ee662a0
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-25 21:33:51 +0700

    PHP CS Fixer

--- src/App/KataBersambung/Group.php
+++ src/App/KataBersambung/Group.php
@@ -9,49 +9,49 @@ namespace App\KataBersambung;
 use App\KataBersambung\Session;
 
 class Group
-{	
-	/**
-	 * @var App\KataBersambung\Session
-	 */
-	private $sess;
+{
+    /**
+     * @var App\KataBersambung\Session
+     */
+    private $sess;
 
-	/**
-	 * Constructor.
-	 */
-	public function __construct($room_id, $room_name, $userid, $uname, $name)
-	{
-		$this->sess = new Session($room_id, $room_name, $userid, $uname, $name);
-	}
+    /**
+     * Constructor.
+     */
+    public function __construct($room_id, $room_name, $userid, $uname, $name)
+    {
+        $this->sess = new Session($room_id, $room_name, $userid, $uname, $name);
+    }
 
-	/**
-	 * @return mixed
-	 */
-	public function open()
-	{
-		return $this->sess->openGroup();
-	}
+    /**
+     * @return mixed
+     */
+    public function open()
+    {
+        return $this->sess->openGroup();
+    }
 
-	/**
-	 * @return bool
-	 */
-	public function join()
-	{
-		return $this->sess->join();
-	}
+    /**
+     * @return bool
+     */
+    public function join()
+    {
+        return $this->sess->join();
+    }
 
-	/**
-	 * @return bool
-	 */
-	public function start()
-	{
-		return $this->sess->start();
-	}
+    /**
+     * @return bool
+     */
+    public function start()
+    {
+        return $this->sess->start();
+    }
 
-	/**
-	 * @return string
-	 */
-	public function input($input)
-	{
-		return $this->sess->input($input);
-	}
+    /**
+     * @return string
+     */
+    public function input($input)
+    {
+        return $this->sess->input($input);
+    }
 }
