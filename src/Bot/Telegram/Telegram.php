b59fe275ad049bc62ef821762a4d3b380562e38d
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-25 21:48:16 +0700

    Update start message

--- src/Bot/Telegram/Telegram.php
+++ src/Bot/Telegram/Telegram.php
@@ -304,7 +304,7 @@ class Telegram implements TelegramContract
                             "command" => "/whatanime",
                             "salt"    => (isset($this->event['message']['photo'][1]) ? $this->getPhotoUrl($this->event['message']['photo'][1]['file_id']) : ((isset($this->event['message']['text']) and filter_var(str_replace(" ", urlencode(" "), $this->event['message']['text']), FILTER_VALIDATE_URL)) ? $this->event['message']['text'] : false)),
                         );
-                } elseif (!isset($this->entities['bot_command']) and substr($a[0], 0, 12) == "#group_party") {
+                } elseif (!isset($this->entities['bot_command']) and substr($a[0], 0, 15) == "#katabersambung") {
                     $this->entities['party'][] = array(
                             "group_in" => strtolower(trim(preg_replace("#[^[:print:]]#", "", $this->event['message']['text'])))
                         );
