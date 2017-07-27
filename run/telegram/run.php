4b9761958cfa103035aac212c93a69657f34d002
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-27 17:43:05 +0700

    nohup out success

--- run/telegram/run.php
+++ run/telegram/run.php
@@ -1,5 +1,5 @@
 #!/usr/bin/php
 <?php
 require __DIR__."/../../vendor/autoload.php";
-isset($argv[1]) and define("webhook_input", $argv[1]);
+isset($argv[1]) and !empty($argv[1]) and define("webhook_input", $argv[1]);
 Bot\Telegram\Bot::run();
