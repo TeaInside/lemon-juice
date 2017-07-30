cd227ace284fc5056f4ca9c1f6092ab855097e74
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-30 19:33:59 +0700

    Mk saver

---
+++ Bot/telegram/Command/Saver.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Telegram\Command;
+
+use Telegram\B;
+use Foundation\DB;
+
+class Saver
+{
+	public function __construct()
+	{
+
+	}
+
+	public function image($image, $title, $caption = "")
+	{
+		$file_name = sha1($image).".jpg";
+		B::table("assets")->insert([
+				"id" => null,
+				"title" => $title,
+				"caption" => $caption,
+				"file_name" => $file_name,
+				"type" => "image",
+				"created_at" => date("Y-m-d H:i:s")
+			]);
+		is_dir("/home/web/bot/public/assets/images/") or shell_exec("sudo mkdir -p /home/web/bot/public/assets/images/");
+		file_put_contents("/home/web/bot/public/assets/images/".$file_name, $image);
+	}
+}
