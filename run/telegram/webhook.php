0a5312c9abfa800febe4e4ac21bfe13a16151be5
Ammar Faizi <ammarfaizi2@gmail.com>
2017-07-30 18:44:00 +0700

    ok

--- run/telegram/webhook.php
+++ run/telegram/webhook.php
@@ -1,4 +1,4 @@
 <?php
 $input = file_get_contents("php://input");
 file_put_contents("text.txt", $input);
-print shell_exec("/usr/bin/php ".__DIR__."/run.php '".base64_encode($input, "'")."'");
+print shell_exec("/usr/bin/php ".__DIR__."/run.php '".base64_encode($input)."'");
