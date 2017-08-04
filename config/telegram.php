96fe304d38ec50dd7f3ebd519fc23be310b94131
Ammar Faizi <ammarfaizi2@gmail.com>
2017-08-04 21:15:04 +0700

    Ok

--- config/telegram.php
+++ config/telegram.php
@@ -1,22 +1,53 @@
 <?php
 
+/**
+ * Timezone.
+ */
 date_default_timezone_set("Asia/Jakarta");
 
+/**
+ * Bot token.
+ */
 define("TOKEN", "448907482:AAGAaT7iP-CUC7xoBeXSyC-mrovzwmYka4w");
 
+/**
+ * Admin and root.
+ */
 define("SUDOERS", "[]");
 define("FORCE_ADMIN", "[]");
 
+/**
+ * Database (MySQL)
+ */
 define("DBHOST", "localhost");
 define("DBPORT", "3306");
 define("DBNAME", "lemon_juice");
 define("DBUSER", "icetea");
 define("DBPASS", "triosemut123");
 
-
+/**
+ * Logs and Storage.
+ */
 define("data", __DIR__."/../public/data");
 define("logs", data."/logs");
 define("storage", data."/storage");
 
+/**
+ * API MyAnimeList.
+ */
 define("MAL_USER", "ammarfaizi2");
 define("MAL_PASS", "triosemut123");
+
+/**
+ * Virtualer
+ */
+$pb = "/home/web/bot/public/";
+$vr = $pb."virtual/";
+define("PHP_VIRTUAL_URL", "https://webhooks.redangel.ga/virtual");
+define("PHP_VIRTUAL_PATH", $vr."php");
+define("C_VIRTUAL_PATH", $vr."c");
+define("JAVA_VIRTUAL_PATH", $vr."java");
+define("RUBY_VIRTUAL_PATH", $vr."ruby");
+define("NODE_VIRTUAL_PATH", $vr."node");
+define("PYTHON_VIRTUAL_PATH", $vr."python");
+
