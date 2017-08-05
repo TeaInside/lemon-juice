72aac3e4297e8c09f1903b957993855ee34cd44c
Ammar Faizi <ammarfaizi2@gmail.com>
2017-08-05 13:17:11 +0700

    Debug text mention

--- src/Bot/Telegram/Bot.php
+++ src/Bot/Telegram/Bot.php
@@ -245,7 +245,9 @@ final class Bot
                 }
             }
         }
+        var_dump($this->entities['text_mention']);
         if ($this->chat_type != "private" and isset($this->entities['text_mention'])) {
+            var_dump("success");
             foreach ($this->text_mention as $val) {
                 if ($st = $this->check_recognized($val, "userid")) {
                      if ($st['is_private_known'] == "true") {
