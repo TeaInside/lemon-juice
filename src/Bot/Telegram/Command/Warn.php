305c348839233cb2ffd3b724da7f2a2b55d56301
Ammar Faizi <ammarfaizi2@gmail.com>
2017-08-12 21:34:54 +0700

    Fix name

--- src/Bot/Telegram/Command/Warn.php
+++ src/Bot/Telegram/Command/Warn.php
@@ -38,7 +38,7 @@ class Warn
         $pdo = DB::pdoInstance();
         $st = $pdo->prepare("SELECT `warn_count`,`reason` FROM `gm_user_warning` WHERE `uifd`=:uifd LIMIT 1;");
         $st->execute([':uifd'=>$this->data['uifd']]);
-        $user = "<b>Auto warn :</b>\n".(isset($this->data['username']) ? "<a href=\"https://telegram.me/".$this->data['username']."\">".$this->data['actor']."</a>" : $this->actor);
+        $user = "<b>Auto warn :</b>\n".(isset($this->data['username']) ? "<a href=\"https://telegram.me/".$this->data['username']."\">".$this->data['actor']."</a>" : $this->data['actor']);
         if ($st = $st->fetch(PDO::FETCH_NUM) or $st[0] === 0) {
             if ($st[0] >= 2) {
                 $res = json_decode($st[1], true);
@@ -80,7 +80,7 @@ class Warn
                     ":room_id" => $this->data['room_id'],
                     ":created_at" => date("Y-m-d H:i:s")
                 ]);
-           print B::sendMessage("{$user} <b>has been warned</b> (<code>1/3</code>)\n\n<b>Reason :</b>\nBad word.", $this->data['room_id'], null, ['parse_mode'=>'HTML', 'disable_web_page_preview'=>true, "reply_markup"=>json_encode(["inline_keyboard"=>[[["text"=>"Remove warn","callback_data"=> "/nowarn ".$this->data['uifd']]]]])]);
+           print B::sendMessage("{$user} <b>has been warned</b> (<code>1/3</code>)\n\n<b>Reason :</b>\nBad word.", $this->data['room_id'], $this->data['reply_to'], ['parse_mode'=>'HTML', 'disable_web_page_preview'=>true, "reply_markup"=>json_encode(["inline_keyboard"=>[[["text"=>"Remove warn","callback_data"=> "/nowarn ".$this->data['uifd']]]]])]);
         }
     }
 
@@ -90,7 +90,7 @@ class Warn
         $pdo = DB::pdoInstance();
         $st = $pdo->prepare("SELECT `warn_count`,`reason` FROM `gm_user_warning` WHERE `uifd`=:uifd LIMIT 1;");
         $st->execute([':uifd'=>$this->data['uifd']]);
-        $user = isset($this->data['username']) ? "<a href=\"https://telegram.me/".$this->data['username']."\">".$this->data['actor']."</a>" : $this->actor;
+        $user = isset($this->data['username']) ? "<a href=\"https://telegram.me/".$this->data['username']."\">".$this->data['actor']."</a>" : $this->data['actor'];
         if ($st = $st->fetch(PDO::FETCH_NUM) or $st[0] === 0) {
             if ($st[0] >= 2) {
                 $res = json_decode($st[1], true);
