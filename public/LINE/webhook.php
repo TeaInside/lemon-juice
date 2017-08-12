f2b1a42bc3e5fc7d07e65ff8868f809ef764f515
Ammar Faizi <ammarfaizi2@gmail.com>
2017-08-12 22:05:52 +0700

    Fix constants

--- public/LINE/webhook.php
+++ public/LINE/webhook.php
@@ -1,3 +1,8 @@
 <?php
 require __DIR__."/../../vendor/autoload.php";
+
+define("data", __DIR__."/data");
+define("logs", data."/logs");
+define("storage", data."/storage");
+
 Bot\LINE\Bot::run("j0BTVSMgvXCFSGvzSQgU19V5G/WHOujP7100ZLUKbiePp9CehOfJEH4YMP/NHKKd5bjJhhTRxBURzPw3Xi939aTamjmDWQJtH81IoHAgFN7xZ6hpDqS8jEVOrL1cSR2HQ9lnAg4zxTWzfEUTex/sXAdB04t89/1O/w1cDnyilFU=", "a710fa6d726c9ca6773a7632d740a0d4");
