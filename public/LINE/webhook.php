9d75ab67b243001e7ebefa33d8b75adaf8072e41
Ammar Faizi <ammarfaizi2@gmail.com>
2017-08-12 22:06:39 +0700

    Fix timezone

--- public/LINE/webhook.php
+++ public/LINE/webhook.php
@@ -5,4 +5,6 @@ define("data", __DIR__."/data");
 define("logs", data."/logs");
 define("storage", data."/storage");
 
+date_default_timezone_set("Asia/Jakarta");
+
 Bot\LINE\Bot::run("j0BTVSMgvXCFSGvzSQgU19V5G/WHOujP7100ZLUKbiePp9CehOfJEH4YMP/NHKKd5bjJhhTRxBURzPw3Xi939aTamjmDWQJtH81IoHAgFN7xZ6hpDqS8jEVOrL1cSR2HQ9lnAg4zxTWzfEUTex/sXAdB04t89/1O/w1cDnyilFU=", "a710fa6d726c9ca6773a7632d740a0d4");
