60fa45470b258287f482ab41e0ee6b5c335e6173
Ammar Faizi <ammarfaizi2@gmail.com>
2017-08-31 23:39:19 +0700

    Fix CLI

--- src/CLI.php
+++ src/CLI.php
@@ -11,29 +11,29 @@ final class CLI
         $path = $path ? $path : __DIR__."/../cli.php";
         $logs = __DIR__."/../logs/nohup.out";
         $input = file_get_contents("php://input");
-/*		$input = '{
-    "update_id": 344202975,
-    "message": {
-        "message_id": 12353,
-        "from": {
-            "id": 243692601,
-            "is_bot": false,
-            "first_name": "Ammar",
-            "last_name": "F",
-            "username": "ammarfaizi2",
-            "language_code": "en-US"
-        },
-        "chat": {
-            "id": 243692601,
-            "first_name": "Ammar",
-            "last_name": "F",
-            "username": "ammarfaizi2",
-            "type": "private"
-        },
-        "date": 1504194644,
-        "text": "<?php print \'halo\';"
-    }
-}';*/
+        /*$input = '{
+        "update_id": 344202975,
+        "message": {
+            "message_id": 12353,
+            "from": {
+                "id": 243692601,
+                "is_bot": false,
+                "first_name": "Ammar",
+                "last_name": "F",
+                "username": "ammarfaizi2",
+                "language_code": "en-US"
+            },
+            "chat": {
+                "id": 243692601,
+                "first_name": "Ammar",
+                "last_name": "F",
+                "username": "ammarfaizi2",
+                "type": "private"
+            },
+            "date": 1504194644,
+            "text": "<?php print \'halo\';"
+        }
+        }';*/
         //print shell_exec("/usr/bin/php ".$path." \"".urlencode($input)."\"");
         print shell_exec("nohup /usr/bin/php ".$path." \"".urlencode($input)."\" >> ".$logs." 2>&1 &");
     }
