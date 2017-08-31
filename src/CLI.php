783f7e45c1ddc6353cf5326c353c1077304e814d
Ammar Faizi <ammarfaizi2@gmail.com>
2017-08-31 17:41:47 +0700

    Fix JSON

--- src/CLI.php
+++ src/CLI.php
@@ -7,6 +7,6 @@ class CLI
 		$path = $path ? $path : __DIR__."/../cli.php";
 		$logs = __DIR__."/../logs/nohup.out";
 		$input = file_get_contents("php://input");
-		print shell_exec("nohup /usr/bin/php ".$path." ".urlencode($input)." >> ".$logs." 2>&1 &");
+		print shell_exec("nohup /usr/bin/php ".$path." \"".urlencode($input)."\" >> ".$logs." 2>&1 &");
 	}
 }
