ca734790d06eaddb5a79666aab2ede905f5bf869
Ammar Faizi <ammarfaizi2@gmail.com>
2017-08-31 18:09:39 +0700

    Fix BIGINT as String

--- load.php
+++ load.php
@@ -1,9 +1,9 @@
 <?php
 require __DIR__."/autoload.php";
 
-
-file_put_contents("qwe", file_get_contents("php://input"));
-/*$app = new Bot();
-$app->run();*/
+/*
+$app = new Bot();
+$app->run();
+*/
 
 CLI::run();
--- src/Handler/MainHandler.php
+++ src/Handler/MainHandler.php
@@ -4,7 +4,56 @@ namespace Handler;
 
 class MainHandler
 {
+	/**
+	 * @var array
+	 */
 	private $event;
+
+	/**
+	 * @var string
+	 */
+	private $type;
+	
+	/**
+	 * @var string
+	 */
+	private $chattype;
+
+	/**
+	 * @var array
+	 */
+	private $from;
+
+	/**
+	 * @var string
+	 */
+	private $actor;
+
+	/**
+	 * @var string
+	 */
+	private $actorcall;
+
+	/**
+	 * @var int
+	 */
+	private $msgid;
+
+	/**
+	 * @var array
+	 */
+	private $chat;
+
+	/**
+	 * @var string
+	 */
+	private $chattitle;
+
+	/**
+	 * @var string
+	 */
+	private $roomid;
+
 
 	/**
 	 * Constructor.
@@ -12,7 +61,7 @@ class MainHandler
 	 */
 	public function __construct($event)
 	{
-
+		$this->event = $event;
 	}
 
 	/**
@@ -20,6 +69,18 @@ class MainHandler
 	 */
 	public function parseEvent()
 	{
-
+		$ev = $this->event;
+		if (isset($ev['message']['text'])) {
+			$this->type = "text";
+			$this->chattype = $ev['message']['chat']['type'];
+			$this->from = $ev['message']['from'];
+			$this->actor = $ev['message']['from']['first_name'].(isset($ev['message']['from']['last_name']) ? " ".$ev['message']['from']['last_name']: "");
+			$this->actorcall = $ev['message']['from']['first_name'];
+			$this->msgid = $ev['message']['message_id'];
+			$this->chat = $ev['message']['chat'];
+			$this->chattitle = $ev['message']['chat']['title'];			
+			$this->roomid = $ev['message']['chat']['id'];
+		}
+		var_dump($ev);
 	}
 }
