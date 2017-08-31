5305512f9527a14d8b56bae5453c61a48be177e8
Ammar Faizi <ammarfaizi2@gmail.com>
2017-08-31 17:37:59 +0700

    Update CLI

--- cli.php
+++ cli.php
@@ -2,5 +2,5 @@
 require __DIR__."/autoload.php";
 if (isset($argv[1])) {
 	$app = new Bot(urldecode($argv[1]));
-	$app->run();	
+	$app->run();
 }
--- src/CLI.php
+++ src/CLI.php
@@ -7,6 +7,7 @@ class CLI
 		$path = $path ? $path : __DIR__."/../cli.php";
 		$logs = __DIR__."/../logs/nohup.out";
 		$input = file_get_contents("php://input");
-		shell_exec("nohup /usr/bin/php ".$path." ".$input." >> ".$logs." 2>&1 &");
+		//$input = json_encode(range(1,1000));
+		print shell_exec("nohup /usr/bin/php ".$path." ".$input." >> ".$logs." 2>&1 &");
 	}
 }
