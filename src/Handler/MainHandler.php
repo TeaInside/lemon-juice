d820ab3ebf020edd49efe6e79bef8f7f2262d1bc
Ammar Faizi <ammarfaizi2@gmail.com>
2017-08-31 15:31:29 +0700

    Create handler

---
+++ src/Handler/MainHandler.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Handler;
+
+class MainHandler
+{
+	private $event;
+
+	/**
+	 * Constructor.
+	 * @param array $event
+	 */
+	public function __construct($event)
+	{
+
+	}
+
+	/**
+	 * Parse Event.
+	 */
+	public function parseEvent()
+	{
+
+	}
+}
