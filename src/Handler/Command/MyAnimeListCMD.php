437db5b159fd99c11efbd703b58866f59d3d994a
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-02 05:55:50 +0700

    Add type hinting

--- src/Handler/Command/MyAnimeListCMD.php
+++ src/Handler/Command/MyAnimeListCMD.php
@@ -28,27 +28,36 @@ class MyAnimeListCMD
      */
     public function __anime($query)
     {
-        $st = new MyAnimeList(MAL_USER, MAL_PASS);
-        $st->search($query);
-        $st->exec();
-        $st = $st->get_result();
-        if (isset($st['entry']['id'])) {
-            $rep = "";
-            $rep.="Hasil pencarian anime :\n<b>{$st['entry']['id']}</b> : {$st['entry']['title']}\n\nBerikut ini adalah anime yang cocok dengan <b>{$val['salt']}</b>.\n\nKetik /idan [spasi] [id_anime] atau balas dengan id anime untuk menampilkan info anime lebih lengkap.";
-        } elseif (is_array($st) and $xz = count($st['entry'])) {
-            $rep = "Hasil pencarian anime :\n";
-            foreach ($st['entry'] as $vz) {
-                $rep .= "<b>".$vz['id']."</b> : ".$vz['title']."\n";
+        if (!empty($query)) {
+            $st = new MyAnimeList(MAL_USER, MAL_PASS);
+            $st->search($query);
+            $st->exec();
+            $st = $st->get_result();
+            if (isset($st['entry']['id'])) {
+                $rep = "";
+                $rep.="Hasil pencarian anime :\n<b>{$st['entry']['id']}</b> : {$st['entry']['title']}\n\nBerikut ini adalah anime yang cocok dengan <b>{$val['salt']}</b>.\n\nKetik /idan [spasi] [id_anime] atau balas dengan id anime untuk menampilkan info anime lebih lengkap.";
+            } elseif (is_array($st) and $xz = count($st['entry'])) {
+                $rep = "Hasil pencarian anime :\n";
+                foreach ($st['entry'] as $vz) {
+                    $rep .= "<b>".$vz['id']."</b> : ".$vz['title']."\n";
+                }
+                $rep.="\nBerikut ini adalah beberapa anime yang cocok dengan <b>{$query}</b>.\n\nKetik /idan [spasi] [id_anime] atau balas dengan id anime untuk menampilkan info anime lebih lengkap.";
+            } else {
+                $rep = "Mohon maaf, anime \"{$query}\" tidak ditemukan !";
             }
-            $rep.="\nBerikut ini adalah beberapa anime yang cocok dengan <b>{$query}</b>.\n\nKetik /idan [spasi] [id_anime] atau balas dengan id anime untuk menampilkan info anime lebih lengkap.";
-        } else {
-            $rep = "Mohon maaf, anime \"{$query}\" tidak ditemukan !";
-        }
-        return B::sendMessage([
+            return B::sendMessage([
                 "chat_id" => $this->hd->chatid,
                 "text" => $rep,
                 "parse_mode" => "HTML",
                 "disable_web_page_preview" => true
             ]);
+        } else {
+            return B::sendMessage([
+                    "chat_id" => $this->hd->chatid,
+                    "text" => "Anime apa yang ingin kamu cari?",
+                    "reply_markup"=>json_encode(["force_reply"=>true,"selective"=>true]),
+                    "reply_to_message_id" => $this->hd->msgid
+                ]);
+        }
     }
 }
