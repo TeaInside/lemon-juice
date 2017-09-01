377bf9c8e895c3e799c887e1dadb8063a305252f
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-01 18:23:48 +0700

    Create DB class and update config

---
+++ src/DB.php
@@ -0,0 +1,19 @@
+<?php
+
+class DB
+{
+	public static function __callStatic($method, $param)
+	{
+		return (new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS))->{$method}(...$param);
+	}
+
+	public static function pdo()
+	{
+		return (new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS));
+	}
+
+	public static function pdoInstance()
+	{
+		return (new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS));	
+	}
+}
