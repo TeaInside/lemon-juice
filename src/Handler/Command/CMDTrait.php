b9f087fc65feec47485325b0f23c3525287e79a7
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-07 18:06:48 +0700

    Debug warn count

--- src/Handler/Command/CMDTrait.php
+++ src/Handler/Command/CMDTrait.php
@@ -54,6 +54,9 @@ trait CMDTrait
         			var_dump($se->errorInfo());
         			die(1);
         		}
+        		print "\n\nwarn count ";
+        		var_dump($sq[0], $st[0]);
+        		print "\n\n";
         		if ($sq[0] >= $st[0]+1) {
         			$a = B::kickChatMember(
 	                    [
